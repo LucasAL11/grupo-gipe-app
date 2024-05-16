e3d7c67deac66f607dbde84d008158cac833b532
Lucas Alexandre Luna <lucasluna1000@gmail.com>
2024-05-16 11:17:13 -0300

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home', 
+        loadComponent: () => import('home').then(m => m.HomeComponent)
+    },
+    {
+        path: 'users', 
+        loadComponent: () => import('users').then(m => m.UsersComponent)
+    },
+    {
+        path: 'security', 
+        loadComponent : () => import('security').then(m => m.SecurityComponent)
+    },
+
+];
